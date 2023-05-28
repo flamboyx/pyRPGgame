727c2e025f32efa4b96b346a7333b5c5d3d8e868
flamboyx <132726172+flamboyx@users.noreply.github.com>
2023-05-28 23:22:28 +1000

    settings.py

--- GameProject/settings.py
+++ GameProject/settings.py
@@ -7,4 +7,11 @@ screen = pygame.display.Info()
 WIDTH = screen.current_w
 HEIGHT = screen.current_h
 FPS = 60
-TILESIZE = 32
+TILESIZE = 64
+
+weapon_data = {
+    'sword': {'cooldown': 100, 'damage': 15, 'graphic': 'images/weapons/sword/up.png'},
+    'scythe': {'cooldown': 100, 'damage': 15, 'graphic': 'images/weapons/scythe/up.png'},
+    'spear': {'cooldown': 100, 'damage': 15, 'graphic': 'images/weapons/spear/up.png'},
+    'heavysword': {'cooldown': 100, 'damage': 15, 'graphic': 'images/weapons/heavysword/up.png'},
+}
