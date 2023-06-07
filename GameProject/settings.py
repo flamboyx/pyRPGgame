8392f3911fc4723ac470fef983f0a67597c3f8bc
flamboyx <132726172+flamboyx@users.noreply.github.com>
2023-06-08 01:43:42 +1000

    settings.py

--- GameProject/settings.py
+++ GameProject/settings.py
@@ -26,16 +26,18 @@ ENERGY_COLOR = '#FFFF3B'
 UI_BORDER_COLOR_ACTIVE = '#E6FFE6'
 
 weapon_data = {
-    'sword': {'cooldown': 100, 'damage': 15, 'graphic': 'images/weapons/sword/up.png'},
-    'scythe': {'cooldown': 100, 'damage': 15, 'graphic': 'images/weapons/scythe/up.png'},
-    'spear': {'cooldown': 100, 'damage': 15, 'graphic': 'images/weapons/spear/up.png'},
-    'heavysword': {'cooldown': 100, 'damage': 15, 'graphic': 'images/weapons/heavysword/up.png'}}
+    'sword': {'cooldown': 100, 'damage': 20, 'graphic': 'images/weapons/sword/up.png'},
+    'scythe': {'cooldown': 150, 'damage': 30, 'graphic': 'images/weapons/scythe/up.png'},
+    'spear': {'cooldown': 115, 'damage': 24, 'graphic': 'images/weapons/spear/up.png'},
+    'heavysword': {'cooldown': 300, 'damage': 40, 'graphic': 'images/weapons/heavysword/up.png'}}
 
 magic_data = {
     'flame': {'strength': 5, 'cost': 20, 'graphic': 'images/test.jpg'},
     'heal': {'strength': 20, 'cost': 10, 'graphic': 'images/icon.png'}}
 
 enemy_data = {
-    'bat': {'health': 100, 'exp': 100, 'damage': 10, 'attack_cooldown': 300, 'attack_type': None, 'attack sound': None, 'speed': 5, 'resistance': 5, 'attack_radius': 40, 'notice_radius': 480},
-    'jaws': {'health': 200, 'exp': 150, 'damage': 30, 'attack_cooldown': 400, 'attack_type': None, 'attack sound': None, 'speed': 2, 'resistance': 3, 'attack_radius': 80, 'notice_radius': 300}
+    'bat': {'health': 100, 'exp': 100, 'damage': 10, 'attack_cooldown': 300, 'attack_type': None,
+            'attack sound': None, 'speed': 5, 'resistance': 5, 'attack_radius': 40, 'notice_radius': 480},
+    'jaws': {'health': 200, 'exp': 150, 'damage': 30, 'attack_cooldown': 500, 'attack_type': None,
+             'attack sound': None, 'speed': 2, 'resistance': 2, 'attack_radius': 70, 'notice_radius': 340}
 }
