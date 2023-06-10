9a62cca9684441d6209d8a96ae972d4d223a27bf
flamboyx <132726172+flamboyx@users.noreply.github.com>
2023-06-10 10:20:46 +1000

    level.py

--- GameProject/level.py
+++ GameProject/level.py
@@ -7,15 +7,16 @@ from tile import Tile
 from weapon import Weapon
 from ui import UI
 from particles import AnimationPlayer
+from magic import MagicPlayer
+from upgrade import Upgrade
 from settings import *
 from support import *
 from random import randint
 
-
-
 class Level:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
+        self.game_paused = False
 
         # sprite group setup
         self.visible_sprites = DepthCameraGroup()
@@ -31,9 +32,12 @@ class Level:
 
         # user interface
         self.ui = UI()
+        self.upgrade = Upgrade(self.player)
 
         # particles
         self.animation_player = AnimationPlayer()
+        self.magic_player = MagicPlayer(self.animation_player)
+
 
     def create_map(self):
         layouts = {
@@ -48,7 +52,9 @@ class Level:
             'flora': import_folder('images/graphics/flora')
         }
 
-        free_space = []
+        free_space = [217, 248, 249, 250, 219, 220, 251, 252, 253, 222, 254, 329, 330, 331, 332, 333, 393, 394, 395,
+                      396, 422, 423, 516, 517, 518, 519, 544, 545, 546, 579, 580, 581, 582, 619, 649, 650, 587, 588,
+                      426, 456]
 
         for style, layout in layouts.items():
             for row_index, row in enumerate(layout):
@@ -120,12 +126,15 @@ class Level:
                             else:
                                 if col == '2': monster_name = 'bat'
                                 elif col == '3': monster_name = 'jaws'
+                                elif col == '4': monster_name = 'poo'
 
                                 Enemy(monster_name,
                                       (x, y),
                                       [self.visible_sprites, self.attackable_sprites],
                                       self.obstacle_sprites,
-                                      self.damage_player)
+                                      self.damage_player,
+                                      self.trigger_death_particles,
+                                      self.add_exp)
 
         self.player = Player((100, 100),
                              [self.visible_sprites],
@@ -138,7 +147,14 @@ class Level:
         self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites])
 
     def create_magic(self, style, strength, cost):
-        print(style)
+        if style == 'heal':
+            self.magic_player.heal(self.player, strength, cost, [self.visible_sprites])
+
+        if style == 'ball':
+            self.magic_player.ball(self.player, cost, [self.visible_sprites, self.attack_sprites])
+
+        if style == 'tentacles':
+            self.magic_player.tentacles(self.player, cost, [self.visible_sprites, self.attack_sprites])
 
     def destroy_attack(self):
         if self.current_attack:
@@ -166,12 +182,26 @@ class Level:
             self.player.hurt_time = pygame.time.get_ticks()
             self.animation_player.create_particles(attack_type, self.player.rect.center, [self.visible_sprites])
 
+    def trigger_death_particles(self, pos, particle_type):
+        self.animation_player.create_particles(particle_type, pos, self.visible_sprites)
+
+    def add_exp(self, amount):
+        self.player.exp += amount
+
+    def toggle_menu(self):
+        self.game_paused = not self.game_paused
+
     def run(self):
         self.visible_sprites.custom_draw(self.player)
-        self.visible_sprites.update()
-        self.visible_sprites.enemy_update(self.player)
-        self.player_attack_logic()
         self.ui.display(self.player)
+
+        if self.game_paused:
+            self.upgrade.display()
+        else:
+            self.visible_sprites.update()
+            self.visible_sprites.enemy_update(self.player)
+            self.player_attack_logic()
+
 
 
 class DepthCameraGroup(pygame.sprite.Group):
